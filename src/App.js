94cb8ff3bbadc70fed35c51311f15d0b7a668976
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-09 05:51:12 -0700

    added render and return to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-//import Form from './Form'
-//import Card from './Card'
+import Form from './Form'
+import Card from './Card'
 
-class App extends Component {
+class App extends React.Component {
+  state = { cards: [], editing: null }
 
+  
+
+  render() {
+    const { editing, cards } = this.state
+
+    return (
+      <div className="container">
+        <Form editing={editing} />
+        { cards.length ? this.showCards() : <h1 className="center">No Cards</h1> }
+      </div>
+    )
+  }
 }
 
 export default App;
